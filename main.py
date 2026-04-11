858eeee0126f3507bf27f46025e713892ca156b4
Sergio <szewczyksergio@gmail.com>
2026-04-11 14:38:11 +0200

    initial devops app

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello from your DevOps app 🚀",
+        "hostname": socket.gethostname()
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
